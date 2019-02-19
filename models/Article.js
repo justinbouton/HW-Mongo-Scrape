76191c1c0a760b417bd6036da5e032689a4e36cf
justbouton <justb1985@gmail.com>
2019-02-18 17:39:43 -0800

    Great progress! Scrap Articles, scrapes and displays on the page.

---
+++ models/Article.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+// schema constructor
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    paragraph: {
+        type: String,
+        required: true
+    },
+    // favorite: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
